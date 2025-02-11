f1ad5dc0fce2fc4708ae56decf9743391cdc8b56
RN <95514704+Half-V@users.noreply.github.com>
2025-02-11 01:56:15 +0000

    jaja

--- Lab-Activity-2/example#8.cpp
+++ Lab-Activity-2/example#8.cpp
@@ -23,7 +23,7 @@ int main() {
 
     sum = A[0][0] + A[1][1] + A[2][2] + A[3][3] + A[4][4];
     prod = A[0][4] * A[1][3] * A[2][2] * A[3][1] * A[4][0];
-    cout <<"The sum of the elements in the main diagonal is: " << sum << endl;
-    cout <<"The product of the elements in the secondary diagonal is: " << prod << endl;
+    cout <<"The sum of the elements in the main diagonal are: " << sum << endl;
+    cout <<"The product of the elements in the secondary diagonal are: " << prod << endl;
     system("pause");
 }
