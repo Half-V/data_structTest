7162ed298b8ffbc14b55f2b512afdc355743f05c
RN <95514704+Half-V@users.noreply.github.com>
2025-04-07 19:15:34 -0700

    Update algo2.cpp

--- algo2.cpp
+++ algo2.cpp
@@ -2,7 +2,7 @@
 #include <string>
 using namespace std;
 
-class SariSariStoreItem {
+class sariSari{
 public:
     string id;
     string name;
@@ -27,7 +27,7 @@ public:
     }
 };
 
-void selectionSort(SariSariStoreItem arr[], int n) {
+void selectionSort(sariSari arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
         int minIndex = i;
         for (int j = i + 1; j < n; j++) {
@@ -36,7 +36,7 @@ void selectionSort(SariSariStoreItem arr[], int n) {
             }
         }
         if (minIndex != i) {
-            SariSariStoreItem temp = arr[i];
+            sariSari temp = arr[i];
             arr[i] = arr[minIndex];
             arr[minIndex] = temp;
         }
@@ -44,21 +44,21 @@ void selectionSort(SariSariStoreItem arr[], int n) {
 }
 
 int main() {
-    SariSariStoreItem items[8];
-    items[0].setData("10245", "mangoes", 20, 60, 100);
-    items[1].setData("20255", "apple", 15, 38, 50);
-    items[2].setData("30356", "C2_red", 30, 360, 200);
-    items[3].setData("12347", "bukopie", 40, 120, 150);
-    items[4].setData("77878", "magic_sarap", 10, 520, 1000);
-    items[5].setData("10255", "C2_green", 25, 360, 300);
-    items[6].setData("12032", "boy_bawang", 18, 400, 140);
-    items[7].setData("11234", "banana", 7, 24, 30);
+    sariSari prod[8];
+    prod[0].setData("10245", "Mangoes", 20, 60, 100);
+    prod[1].setData("20255", "Apple", 15, 38, 50);
+    prod[2].setData("30356", "C2_Red", 30, 360, 200);
+    prod[3].setData("12347", "Bukopie", 40, 120, 150);
+    prod[4].setData("77878", "Magic_Sarap", 10, 520, 1000);
+    prod[5].setData("10255", "C2_Green", 25, 360, 300);
+    prod[6].setData("12032", "Boy_Bawang", 18, 400, 140);
+    prod[7].setData("11234", "Banana", 7, 24, 30);
 
- selectionSort(items, 8);
+ selectionSort(prod, 8);
 
-    cout << "Products sorted by shelf life in ascending order:" << endl;
+    cout << "Products by shelf life in ascending order:" << endl;
     for (int i = 0; i < 8; i++) {
-        cout << items[i].name << endl;
+        cout << prod[i].name << endl;
     }
 
     return 0;
